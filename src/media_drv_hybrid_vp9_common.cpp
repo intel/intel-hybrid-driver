6433a170011a8db19b73e8dc597136ff137a5fd7
Zhao Yakui <yakui.zhao@intel.com>
2014-12-15 13:42:36 +0800

    Export the initialization API of Hybrid VP9 decoding
    Change-Id: I939a851fa1bca1bd6c76cafe431501d388987fa8
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

---
+++ src/media_drv_hybrid_vp9_common.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2014 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ *      Zhao Yakui <yakui.zhao@intel.com>
+ *
+ */
+
+#include "media_drv_hybrid_vp9_common.h"
+#include "decode_hybrid_vp9.h"
+
+
+extern struct hw_context *
+media_hybrid_dec_hw_context_init(VADriverContextP ctx, struct object_config *obj_config)
+{
+  void *vp9_context = calloc(1, sizeof(hybrid_vp9_hw_context));
+
+  Intel_HybridVp9Decode_Initialize(ctx, (void *)vp9_context);
+
+  return (struct hw_context *)(vp9_context);
+}
