294baa941c084c45e5c928ca86b9a36b9cd28921
Zhao Yakui <yakui.zhao@intel.com>
2015-01-21 01:57:43 +0800

    Update the copyright of GPU binary shader for vp9 decoding
    Follow the copyright in media_drv_kernel_g8.c/g7.c
    
    Change-Id: I634faea68319986f94a1b7a1407cd4a4e1f7968d
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

--- src/vp9h/intel_hybrid_vp9_kernel_g75.cpp
+++ src/vp9h/intel_hybrid_vp9_kernel_g75.cpp
@@ -1,30 +1,36 @@
-/*
- * Copyright © 2014 Intel Corporation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sub license, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice (including the
- * next paragraph) shall be included in all copies or substantial portions
- * of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
- * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * Authors:
- *     Zhao Yakui <yakui.zhao@intel.com>
- *
- */
+/* 
+* Copyright (c) 2014, Intel Corporation
+*
+* All rights reserved.
+* Redistribution.  Redistribution and use in binary form, without modification, are 
+* permitted provided that the following conditions are met:
+* Redistributions must reproduce the above copyright notice and the following 
+* disclaimer in the documentation and/or other materials provided with the 
+* distribution. 
+* Neither the name of Intel Corporation nor the names of its suppliers may be used 
+* to endorse or promote products derived from this software without specific prior 
+* written permission. 
+* No reverse engineering, decompilation, or disassembly of this software is permitted.
+* Limited patent license.  Intel Corporation grants a world-wide, royalty-free, 
+* nonexclusive license under patents it now or hereafter owns or controls to make, have made, 
+* use, import, offer to sell and sell (.Utilize.) this software, but solely to the extent that any 
+* such patent is necessary to Utilize the software alone.  The patent license shall not apply 
+* to any combinations which include this software.  No hardware per se is licensed here under.
+
+* DISCLAIMER.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT 
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
+* OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+* NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 unsigned int Vp9Deblock_g75_size = 0x0002141e;
 unsigned int Vp9Deblock_g75 [] = 
